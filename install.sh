228a6252f7acd38c0f0805d73038fb2a88b3c1d8
HTsuyoshiy <henriquetsuyoshiy@gmail.com>
2021-10-05 20:33:45 -0300

    organize files to install with stow

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+function basic_install() {
+    # Create folders
+    mkdir -p ~/.config/{nvim, qutebrowser}/
+}
+
+function full_install() {
+    # Create folders
+    mkdir -p ~/.config/{nvim, qutebrowser, spotify-tui}/
+}
+
---
+++ teste/test.sh
@@ -0,0 +1,1 @@
+[ -f /etc/hostsa ] && echo "Found" || echo "Not found"
