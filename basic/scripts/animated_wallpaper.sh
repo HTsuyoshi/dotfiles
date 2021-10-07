e301d67922a6802aaceb841100d0b2d95f0a661d
HTsuyoshiy <henriquetsuyoshiy@gmail.com>
2021-10-07 00:18:35 -0300

    remove some unnecessary functions

--- basic/scripts/animated_wallpaper.sh
+++ basic/scripts/animated_wallpaper.sh
@@ -3,7 +3,8 @@
 function animated_wallpapers() {
     pkill mpv
 
-	xwinwrap_args="-ni -b -nf -un -o 1.0 -ov -debug"
+	#xwinwrap_args="-ni -b -nf -fs" # 1 screen
+	xwinwrap_args="-ni -b -nf -ov"
 	mpv_args="-wid WID --loop --no-audio --no-resume-playback --panscan=1.0"
 
     folder="$HOME/opt/animated_wallpapers/"
@@ -15,8 +16,8 @@ function animated_wallpapers() {
     margin="--video-align-x=-1 --video-align-y=-1"
     scale="--video-scale-x=0.86 --video-scale-y=0.86"
 
-    nice xwinwrap -g $screen_1 $xwinwrap_args -- mpv $mpv_args $folder$random_1 &
-    nice xwinwrap -g $screen_2 $xwinwrap_args -- mpv $scale $margin $mpv_args $folder$random_2 &
+    nice -n 15 xwinwrap -g $screen_1 $xwinwrap_args -- mpv $mpv_args $folder$random_1 &
+    nice -n 15 xwinwrap -g $screen_2 $xwinwrap_args -- mpv $scale $margin $mpv_args $folder$random_2 &
 }
 
 until $(echo xwininfo -root) | grep "IsViewable"; do :; done
