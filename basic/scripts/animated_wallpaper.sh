bf1bf1107fd772be512f9347c041c7be4ec46c18
HTsuyoshiy <henriquetsuyoshiy@gmail.com>
2021-10-14 05:20:06 -0300

    refact(basic): choose wallppaer

--- basic/scripts/animated_wallpaper.sh
+++ basic/scripts/animated_wallpaper.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 function animated_wallpapers() {
-    pkill mpv
+    pkill mpv 2> /dev/null
+
+    local wallpaper="perfect"
+    [[ ! -z "$1" ]] && wallpaper="$1"
 
     #xwinwrap_args="-ni -b -nf -fs" # 1 screen
-    xwinwrap_args="-ni -b -nf -ov"
-    mpv_args="-wid WID --loop --no-audio --no-resume-playback --panscan=1.0"
+    local xwinwrap_args="-ni -b -nf -ov -d"
+    local mpv_args="-wid WID --loop --no-audio --no-resume-playback --panscan=1.0"
 
-    folder="$HOME/opt/animated_wallpapers/perfect/"
-    random_1=$(ls $folder |sort -R |tail -n 1)
-    random_2=$(ls $folder |sort -R |tail -n 1)
+    local folder="$HOME/opt/animated_wallpapers/$wallpaper/"
+    local random_1=$(ls $folder |sort -R |tail -n 1)
+    local random_2=$(ls $folder |sort -R |tail -n 1)
 
-    screen_1="1920x1080"
-    screen_2="1600x900+1920"
-    margin="--video-align-x=-1 --video-align-y=-1"
-    scale="--video-scale-x=0.86 --video-scale-y=0.86"
+    local screen_1="1920x1080"
+    local screen_2="1600x900+1920"
+    local align="--video-align-x=-1 --video-align-y=-1"
+    local scale="--video-scale-x=0.86 --video-scale-y=0.86"
 
     nice -n 15 xwinwrap -g $screen_1 $xwinwrap_args -- mpv $mpv_args $folder$random_1 &
-    nice -n 15 xwinwrap -g $screen_2 $xwinwrap_args -- mpv $scale $margin $mpv_args $folder$random_2 &
+    nice -n 15 xwinwrap -g $screen_2 $xwinwrap_args -- mpv $scale $align $mpv_args $folder$random_2 & 
 }
 
-until $(echo xwininfo -root) | grep "IsViewable"; do :; done
-animated_wallpapers
+until $(echo xwininfo -root) | grep "IsViewable" > /dev/null; do :; done
+animated_wallpapers "$1" $
