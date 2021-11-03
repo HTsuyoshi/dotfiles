48ac419b369e8f7c7b0c67185d21a1c1aa8352ae
HTsuyoshiy <henriquetsuyoshiy@gmail.com>
2021-11-03 13:00:49 -0300

    refact(dwm_bar.sh): refact and add battery func

--- basic/scripts/dwm_bar.sh
+++ basic/scripts/dwm_bar.sh
@@ -1,78 +1,82 @@
 #!/bin/sh
 
-# Colors
-blue=#61afef
-blue1=#509ede
-red=#db4b4b
-purple=#c678dd
-purple1=#a456bb
-green=#98c379
-green1=#87b268
-yellow=#e0af68
-yellow1=#da8d57
-gray=#adbac7
-black=#21252b
-black=#32363c
-black1=#111111
-white=#e4e4e4
+print_icon() {
+    [ "$INVERT" = "on" ] && inic="^c$white^^b$1^" && fim="^b$black^^c$1^"
+    [ "$INVERT" = "off" ] && inic="^c$1^^b$black1^" && fim="^b$black1^^c$1^"
 
-dwm_mem () {
-	# get all the infos first to avoid high resources usage
-	free_output=$(free -h | grep Mem)
-	# Used and total memory
-	MEMUSED=$(echo $free_output | awk '{print $3}')
-	MEMTOT=$(echo $free_output | awk '{print $2}')
-    local color="$red"
+    [ "$IDENTIFIER" = "unicode" ] && symb=" $2 " || symb=" $3 "
 
-    printf "^c$white^^b$color^"
-
-	if [ "$IDENTIFIER" = "unicode" ]; then
-		printf "  "
-	else
-		printf " MEM "
-	fi
-
-    printf "^b$black^^c$color^ %s/%s ^b$black1^" "$MEMUSED" "$MEMTOT"
+    printf "$inic$symb$fim"
 }
 
 dwm_temp () {
-	TEMP=$(sensors | grep "Package id 0" | awk '{print $4}' | cut -c 2-5)
+    TEMP=$(sensors | grep "Package id 0" | awk '{print $4}' | cut -c 2-5)
 
     local color="$green"
     local color1="$green1"
-    printf "^c$white^^b$color^"
 
-	if [ "$IDENTIFIER" = "unicode" ]; then
-		printf " 糖 "
-	else
-		printf " C "
-	fi
+    local icon="糖"
+    local no_icon="C"
 
-    if (( $(echo "$TEMP > 70.0" |bc -l) )); then
-        printf "^b$color1^^c$white^ %s ^b$black1^" "$TEMP"
+    print_icon "$color" "$icon" "$no_icon"
+
+    if (( $(echo "$TEMP > 80.0" | bc -l) )); then
+        printf " $TEMP ^b$black1^"
     else
-        printf "^b$black^^c$color^ %s ^b$black1^" "$TEMP"
+        printf " $TEMP ^b$black1^"
     fi
+}
+
+dwm_mem () {
+    # get all the infos first to avoid high resources usage
+    free_output=$(free -h | grep Mem) # Used and total memory
+    MEMUSED=$(echo $free_output | awk '{print $3}')
+    MEMTOT=$(echo $free_output | awk '{print $2}')
+
+    local color="$red"
+
+    local icon=""
+    local no_icon="MEM"
+
+    print_icon "$color" "$icon" "$no_icon"
+
+    printf " $MEMUSED/$MEMTOT ^b$black1^" 
 }
 
 internet_usage() {
     local color="$yellow"
     local color1="$yellow1"
+
+    local icon_color="^c$color^^b$black1^"
+
     internet="enp3s0"
-    printf "^c$white^^c$color^"
-    awk '{
-    if(l1){
+
+    [ "$INVERT" = "on" ] && icon_color="^c$white^^b$color^"
+
+    awk -v color="$yellow" \
+        -v color1="$yellow1" \
+        -v white="$white" \
+        -v black="$black" \
+        -v black1="$black1" \
+        -v icon_color="$icon_color"\
+    '{if(l1){
+
+        printf "%s", icon_color;
+
         if(($2-l1)/(1024*1024) < 1) {
-            printf "^b#e0af68^^b$yellow^^c#e4e4e4^  ^b#32363c^^c#e0af68^ %.1f Kb/s ^b#111111^ ", ($2-l1)/1024;
+            printf "  ^b%s^^c%s^ %.1f Kb/s ^b%s^ ", black, color, ($2-l1)/1024, black1;
         } else {
-            printf "^b#e0af68^^c#e4e4e4^  ^b#da8d57^ %2.1f Mb/s ^b#111111^ ", ($2-l1)/(1024*1024);
+            printf "  ^b%s^ %2.1f Mb/s ^b%s^ ", color1, ($2-l1)/(1024*1024), black1;
         }
+
+        printf "%s", icon_color;
+
         if(($10-l2)/(1024*1024) < 1) {
-            printf "^b#e0af68^^c#e4e4e4^ 祝 ^b#32363c^^c#e0af68^ %.1f Kb/s ^b#111111^", ($10-l2)/1024;
+            printf " 祝 ^b%s^^c%s^ %.1f Kb/s ^b%s^", black, color, ($10-l2)/1024, black1;
         } else {
-            printf "^b#e0af68^^c#e4e4e4^ 祝 ^b#da8d57^ %2.1f Mb/s ^b#111111^", ($2-l1)/(1024*1024)
+            printf " 祝 ^b%s^ %2.1f Mb/s ^b%s^", black1, ($2-l1)/(1024*1024), black1
         }}
-    else{l1=$2; l2=$10;}}' \
+    else {l1=$2; l2=$10;} }' \
         <(grep $internet /proc/net/dev) <(sleep 1; grep $internet /proc/net/dev)
 }
 
@@ -81,36 +85,41 @@ dwm_vpn () {
     HTB=$(nmcli -a | grep '10.10.15.7' | cut -c 8-17 | uniq)
 
     local color="$blue"
-    printf "^c$white^^b$color^"
-    if [ "$THM" = "10.2.16.237" ]; then
-		        printf " 說 ^b$blue1^^c$white^ THM %s ^b$black1^" "$THM"
-        else if [ "$HTB" = "10.10.15.7" ]; then
-		        printf " 說 ^b$blue1^^c$white^ HTB %s ^b$black1^" "$HTB"
-            else
-		    printf " ﲁ ^b$black^^c$color^ %s ^b$black1^" "NO VPN"
-        fi
-    fi
+    local color1="$blue1"
+
+    local icon='ﲁ'
+    local no_icon='VPN'
+    local message='NO VPN'
+
+    [ "$THM" = '10.2.16.237' ] && icon='說' && message="THM $THM"
+    [ "$HTB" = '10.10.15.7' ] && icon='說' && message="HTB $HTB"
+
+    print_icon "$color" "$icon" "$no_icon"
+
+    printf " $message ^b$black1^"
 }
 
 dwm_date () {
     local color="$purple"
     local color1="$purple1"
-    printf "^c$white^^b$color^"
-    if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "  ^b$color1^ %s ^b$black1^^b$black1^ " "$(date "+%a, %d/%m/%y %H:%M")"
-    else
-        printf " DAT ^b$color1^ %s ^b$black1^^b$black1^ " "$(date "+%a, %d/%m/%y %H:%M")"
-    fi
+
+    local icon=""
+    local no_icon="DAT"
+
+    print_icon "$color" "$icon" "$no_icon"
+
+
+    [ "$INVERT" = "on" ] && \
+        BG="^b$color1^^c$white^" || \
+        BG="^b$black1^^c$color^"
+
+    [ "$DARK" = "on" ] && BG="^b$black1^^c$color^"
+
+    printf "$BG $(date "+%a, %d/%m/%y %H:%M") ^b$black1^"
 }
 
-complete_bar() {
-    LOC=$(readlink -f "$0")
-    DIR=$(dirname "$LOC")
-
-    while true
-    do
-        xsetroot -name "$(dwm_mem) $(dwm_temp) $(internet_usage) $(dwm_vpn) $(dwm_date)" && sleep 1
-    done
+battery() {
+    upower -i $(upower -e | grep 'BAT') | grep -E "state|to\ full|percentage"
 }
 
 bar() {
@@ -123,6 +132,25 @@ bar() {
     done
 }
 
+# Settings
 export IDENTIFIER="unicode"
+export INVERT="on"
+export DARK="on"
+
+# Colors
+blue=#61afef
+blue1=#509ede
+red=#db4b4b
+purple=#c678dd
+purple1=#a456bb
+green=#98c379
+green1=#87b268
+yellow=#e0af68
+yellow1=#da8d57
+black=#32363c
+black1=#111111
+white=#e4e4e4
+
+[ $DARK = 'on' ] && black="$black1"
 
 bar
