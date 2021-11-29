f1e90fe44fba83287953c5304a1a38a4917ce0f8
HTsuyoshiy <henriquetsuyoshiy@gmail.com>
2021-11-29 02:34:27 -0300

    refact(feh_bg.sh): can choose any wallpaper folder

--- basic/scripts/feh_bg.sh
+++ basic/scripts/feh_bg.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
-set_wallpaper() {
-	local folder=~/opt/my-graphical-interface/wallpaper
-	local all='feh --recursive --bg-fill --bg-scale --randomize'
-	local local='feh --bg-fill --bg-scale --randomize'
-	
-	if [ -z $1 ]; then
-		$all $folder
-	else
-		case $1 in 
-			eits) $local $folder/eits;;
-			anastasia) $local $folder/Anastasia;;
-			jeanne) $local $folder/jeanne;;
-			red) $local $folder/Red;;
-			misc) $local $folder/misc;;
-			games) $all $folder/games;;
-			rpgmaker) $local $folder/games/rpgmaker;;
-			rpgmaker1) $local $folder/games/rpgmaker/inGame;;
-			yes) $all $folder/yes;;
-			astolfo) $all $folder/astolfo;;
-			*) $all $folder;;
-		esac
-	fi
+folder="$HOME/opt/wallpapers/wallpaper"
+
+function usage() {
+    local CategoryList=$(ls $folder)
+
+    echo ''
+    echo 'These categorys are avaliable:'
+    echo ''
+
+    for category in "${CategoryList[@]}";
+    do
+        echo "$category"
+    done
+
+    echo ''
+    echo 'Run:'
+    echo 'feh_bg.sh your_category'
+    echo ''
 }
 
-set_wallpaper $1
+set_wallpaper() {
+	local all='feh --recursive --bg-fill --bg-scale --randomize'
+	
+	$all $folder/$1
+}
+
+[[ -z $1 ]] && usage || set_wallpaper $1
