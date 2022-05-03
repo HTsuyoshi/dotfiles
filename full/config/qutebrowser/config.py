85c86a148dbf7c8b35ba220079b47326289dbf11
HTsuyoshi <henriquetsuyoshiy@gmail.com>
2022-05-02 22:57:13 -0300

    refact(qutebrowser): change start page to `startpage.com`

--- full/config/qutebrowser/config.py
+++ full/config/qutebrowser/config.py
@@ -1,3 +1,7 @@
 
 config.load_autoconfig()
 config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach mpv --force-window yes {hint-url}')
+
+search_engine = 'www.startpage.com'
+c.url.default_page = search_engine
+c.url.start_pages = [search_engine]
